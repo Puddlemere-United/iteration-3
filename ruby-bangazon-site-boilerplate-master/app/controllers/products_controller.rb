42772e2783fc218df6f8483326d9aff506dd1711
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-11-15 12:02:23 -0600

    added resources for the controllers

--- ruby-bangazon-site-boilerplate-master/app/controllers/products_controller.rb
+++ ruby-bangazon-site-boilerplate-master/app/controllers/products_controller.rb
@@ -1,2 +1,19 @@
 class ProductsController < ApplicationController
+	
+	def index
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+	end
+
+	private
+
+		def product_params
+			params.require(:product).permit(:title, :description, :price, :quantity, :sold, :local_delivery)
+		end
 end
