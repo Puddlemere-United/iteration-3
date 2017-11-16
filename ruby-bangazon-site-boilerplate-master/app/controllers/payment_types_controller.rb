c4aa4283de4362e6f91e2a13114266e850faeaa1
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-11-16 09:07:05 -0600

    added erbs for payment types, added methods to payment type

--- ruby-bangazon-site-boilerplate-master/app/controllers/payment_types_controller.rb
+++ ruby-bangazon-site-boilerplate-master/app/controllers/payment_types_controller.rb
@@ -1,10 +1,33 @@
 class PaymentTypesController < ApplicationController
 
+	def index
+	end
+
 	def new
 		@payment_type = PaymentType.new
 	end
 
-	def show
-	 	
+	def create
+		@payment_type = PaymentType.new(payment_type_params)
+		if @payment_type.save
+		redirect_to @payment_type
+		else
+			render 'new'
+		end
 	end
+
+	def show
+		@payment_type = PaymentType.find(params[:id])
+	end
+
+	def destroy
+		@payment_type = PaymentType.find(params[:id])
+        @payment_type.destroy
+        redirect_to payment_type_path
+	end
+
+	private
+    def payment_type
+        params.require(:payment_type).permit(:title, :account_number)
+    end
 end
