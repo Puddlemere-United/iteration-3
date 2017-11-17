3021ab833023638d4adafb837fcbda3baf560309
jordanberghofer <jordan.berghofer@gmail.com>
2017-11-17 14:26:13 -0600

    Ticket 12 completed. Also added functionality to ensure a user must me logged in, in order to access shopping cart

--- ruby-bangazon-site-boilerplate-master/app/helpers/application_helper.rb
+++ ruby-bangazon-site-boilerplate-master/app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
-  private
-
-  def logged_in?
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+	
+	
+	private	
+	def logged_in?
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		# session[:user_id]
+	end
 end
