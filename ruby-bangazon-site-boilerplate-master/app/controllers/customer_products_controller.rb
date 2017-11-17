97f5e8cfc6f5e8b59c6c0ee0152f2a5c3a04440b
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-11-17 09:11:38 -0600

    #19 WIP

--- ruby-bangazon-site-boilerplate-master/app/controllers/customer_products_controller.rb
+++ ruby-bangazon-site-boilerplate-master/app/controllers/customer_products_controller.rb
@@ -1,4 +1,17 @@
 class CustomerProductsController < ApplicationController
 
+	def show
+    end
+
+	def index
+    	@products = Product.where(:user_id => session[:user_id])
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+
+		redirect_to customer_products_path
+	end
 
 end
