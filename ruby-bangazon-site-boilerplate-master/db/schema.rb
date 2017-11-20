31ce8082c714425234767133451ec9c0392a97c3
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-11-20 15:00:23 -0600

    fix db

--- ruby-bangazon-site-boilerplate-master/db/schema.rb
+++ ruby-bangazon-site-boilerplate-master/db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 20171119032159) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_orders_products_on_order_id"
-    t.index ["product_id"], name: "index_orders_products_on_product_id"
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
