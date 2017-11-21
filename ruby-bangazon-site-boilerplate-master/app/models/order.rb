5f6db85af04256971c9a415f64404b79b00d4e4a
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-11-21 10:19:15 -0600

    able to add products to an order

--- ruby-bangazon-site-boilerplate-master/app/models/order.rb
+++ ruby-bangazon-site-boilerplate-master/app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
 	belongs_to :user
-	belongs_to :payment_type
+	belongs_to :payment_type, optional: true
 	has_many :order_products
 	has_many :products, through: :order_products
 end
